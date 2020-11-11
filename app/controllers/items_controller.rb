652f05c355213c88a9c9bc1e348586f59d55e904
taiyonishino <taiyo0807@icloud.com>
2020-11-12 00:11:03 +0900

    商品編集表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,22 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to root_path
+    end
+  end
+
+  def update
+      @item = Item.find(params[:id])
+   if @item.update(item_params)
+      redirect_to item_path(@item.id)
+   else
+       render :edit
+     end
+   end
+
 private
   def item_params
     params.require(:item).permit(:image ,:name, :explanation ,:category_id, :status_id, :burden_id, :prefecture_id, :days_id, :prace,).merge(user_id: current_user.id)
