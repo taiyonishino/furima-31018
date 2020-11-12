e0b08a9c3438a8c7509869f903d5594f861fd349
taiyonishino <taiyo0807@icloud.com>
2020-11-12 11:54:28 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,6 +36,12 @@ class ItemsController < ApplicationController
      end
    end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
+
 private
   def item_params
     params.require(:item).permit(:image ,:name, :explanation ,:category_id, :status_id, :burden_id, :prefecture_id, :days_id, :prace,).merge(user_id: current_user.id)
